4bc2e1cab298262b27ff39cea69c09ffae5530ae
m-manu619 <mmanohar619@gmail.com>
2024-11-22 09:57:07 +0530

    Initial commit for frontend

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div style={{ textAlign: 'center', padding: '20px' }}>
+            <h1>E-Commerce Application</h1>
+            <p>Welcome to our e-commerce platform powered by AWS DevOps.</p>
+        </div>
+    );
+}
+
+export default App;
